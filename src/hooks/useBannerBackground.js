faf7b9e0431a33deaf1d6845c7cc8def697076e5
hyhong-code <hong961127@gmail.com>
2021-02-21 20:12:41 -0800

    Full screen banner

--- src/hooks/useBannerBackground.js
+++ src/hooks/useBannerBackground.js
@@ -1,257 +1,250 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import debounce from "@/utils/debounce";
 
 const useBannerBackground = () => {
-	useEffect(() => {
-		//Helpers
-		function lineToAngle(x1, y1, length, radians) {
-			let x2 = x1 + length * Math.cos(radians),
-				y2 = y1 + length * Math.sin(radians);
-			return { x: x2, y: y2 };
-		}
+  const [windowInnerWidth, setWindowInnerWidth] = useState(0);
+  useEffect(() => {
+    const resize = debounce(function () {
+      setWindowInnerWidth(window.innerWidth);
+    }, 100);
+    window.addEventListener("resize", resize);
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, []);
 
-		function randomRange(min, max) {
-			return min + Math.random() * (max - min);
-		}
+  useEffect(() => {
+    //Helpers
+    function lineToAngle(x1, y1, length, radians) {
+      let x2 = x1 + length * Math.cos(radians),
+        y2 = y1 + length * Math.sin(radians);
+      return { x: x2, y: y2 };
+    }
 
-		function degreesToRads(degrees) {
-			return (degrees / 180) * Math.PI;
-		}
+    function randomRange(min, max) {
+      return min + Math.random() * (max - min);
+    }
 
-		//Particle
-		let particle = {
-			x: 0,
-			y: 0,
-			vx: 0,
-			vy: 0,
-			radius: 0,
+    function degreesToRads(degrees) {
+      return (degrees / 180) * Math.PI;
+    }
 
-			create: function (x, y, speed, direction) {
-				let obj = Object.create(this);
-				obj.x = x;
-				obj.y = y;
-				obj.vx = Math.cos(direction) * speed;
-				obj.vy = Math.sin(direction) * speed;
-				return obj;
-			},
+    //Particle
+    let particle = {
+      x: 0,
+      y: 0,
+      vx: 0,
+      vy: 0,
+      radius: 0,
 
-			getSpeed: function () {
-				return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-			},
+      create: function (x, y, speed, direction) {
+        let obj = Object.create(this);
+        obj.x = x;
+        obj.y = y;
+        obj.vx = Math.cos(direction) * speed;
+        obj.vy = Math.sin(direction) * speed;
+        return obj;
+      },
 
-			setSpeed: function (speed) {
-				let heading = this.getHeading();
-				this.vx = Math.cos(heading) * speed;
-				this.vy = Math.sin(heading) * speed;
-			},
+      getSpeed: function () {
+        return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+      },
 
-			getHeading: function () {
-				return Math.atan2(this.vy, this.vx);
-			},
+      setSpeed: function (speed) {
+        let heading = this.getHeading();
+        this.vx = Math.cos(heading) * speed;
+        this.vy = Math.sin(heading) * speed;
+      },
 
-			setHeading: function (heading) {
-				let speed = this.getSpeed();
-				this.vx = Math.cos(heading) * speed;
-				this.vy = Math.sin(heading) * speed;
-			},
+      getHeading: function () {
+        return Math.atan2(this.vy, this.vx);
+      },
 
-			update: function () {
-				this.x += this.vx;
-				this.y += this.vy;
-			},
-		};
+      setHeading: function (heading) {
+        let speed = this.getSpeed();
+        this.vx = Math.cos(heading) * speed;
+        this.vy = Math.sin(heading) * speed;
+      },
 
-		//Canvas and settings
-		let canvas = document.querySelector(".section-code-banner__background"),
-			context = canvas.getContext("2d"),
-			width = (canvas.width = window.innerWidth),
-			height = (canvas.height = window.innerHeight),
-			stars = [],
-			shootingStars = [],
-			layers = [
-				{ speed: 0.015, scale: 0.2, count: 160 },
-				{ speed: 0.03, scale: 0.5, count: 25 },
-				{ speed: 0.05, scale: 0.75, count: 15 },
-			],
-			starsAngle = 145,
-			shootingStarSpeed = {
-				min: 15,
-				max: 20,
-			},
-			shootingStarOpacityDelta = 0.01,
-			trailLengthDelta = 0.01,
-			shootingStarEmittingInterval = 1000,
-			shootingStarLifeTime = 500,
-			maxTrailLength = 350,
-			starBaseRadius = 2,
-			shootingStarRadius = 3,
-			paused = false;
+      update: function () {
+        this.x += this.vx;
+        this.y += this.vy;
+      },
+    };
 
-		//Create all stars
-		for (let j = 0; j < layers.length; j += 1) {
-			let layer = layers[j];
-			for (let i = 0; i < layer.count; i += 1) {
-				let star = particle.create(randomRange(0, width), randomRange(0, height), 0, 0);
-				star.radius = starBaseRadius * layer.scale;
-				star.setSpeed(layer.speed);
-				star.setHeading(degreesToRads(starsAngle));
-				stars.push(star);
-			}
-		}
+    //Canvas and settings
+    let canvas = document.querySelector(".section-code-banner__background"),
+      context = canvas.getContext("2d"),
+      width = (canvas.width = window.innerWidth),
+      height = (canvas.height = window.innerHeight),
+      stars = [],
+      shootingStars = [],
+      layers = [
+        { speed: 0.015, scale: 0.2, count: 160 },
+        { speed: 0.03, scale: 0.5, count: 25 },
+        { speed: 0.05, scale: 0.75, count: 15 },
+      ],
+      starsAngle = 145,
+      shootingStarSpeed = {
+        min: 15,
+        max: 20,
+      },
+      shootingStarOpacityDelta = 0.01,
+      trailLengthDelta = 0.01,
+      shootingStarEmittingInterval = 1500,
+      shootingStarLifeTime = 500,
+      maxTrailLength = 350,
+      starBaseRadius = 2,
+      shootingStarRadius = 3;
 
-		function createShootingStar() {
-			let shootingStar = particle.create(
-				randomRange(width / 2, width),
-				randomRange(0, height / 2),
-				0,
-				0
-			);
-			shootingStar.setSpeed(randomRange(shootingStarSpeed.min, shootingStarSpeed.max));
-			shootingStar.setHeading(degreesToRads(starsAngle));
-			shootingStar.radius = shootingStarRadius;
-			shootingStar.opacity = 0;
-			shootingStar.trailLengthDelta = 0;
-			shootingStar.isSpawning = true;
-			shootingStar.isDying = false;
-			shootingStars.push(shootingStar);
-		}
+    //Create all stars
+    for (let j = 0; j < layers.length; j += 1) {
+      let layer = layers[j];
+      for (let i = 0; i < layer.count; i += 1) {
+        let star = particle.create(randomRange(0, width), randomRange(0, height), 0, 0);
+        star.radius = starBaseRadius * layer.scale;
+        star.setSpeed(layer.speed);
+        star.setHeading(degreesToRads(starsAngle));
+        stars.push(star);
+      }
+    }
 
-		function killShootingStar(shootingStar) {
-			setTimeout(function () {
-				shootingStar.isDying = true;
-			}, shootingStarLifeTime);
-		}
+    function createShootingStar() {
+      let shootingStar = particle.create(
+        randomRange(width / 2, width),
+        randomRange(0, height / 2),
+        0,
+        0
+      );
+      shootingStar.setSpeed(randomRange(shootingStarSpeed.min, shootingStarSpeed.max));
+      shootingStar.setHeading(degreesToRads(starsAngle));
+      shootingStar.radius = shootingStarRadius;
+      shootingStar.opacity = 0;
+      shootingStar.trailLengthDelta = 0;
+      shootingStar.isSpawning = true;
+      shootingStar.isDying = false;
+      shootingStars.push(shootingStar);
+    }
 
-		function update() {
-			if (!paused) {
-				context.clearRect(0, 0, width, height);
-				context.fillStyle = "rgba(255, 255, 255, 0)";
-				context.fillRect(0, 0, width, height);
-				context.fill();
-				for (let i = 0; i < stars.length; i += 1) {
-					let star = stars[i];
-					star.update();
-					drawStar(star);
-					if (star.x > width) {
-						star.x = 0;
-					}
-					if (star.x < 0) {
-						star.x = width;
-					}
-					if (star.y > height) {
-						star.y = 0;
-					}
-					if (star.y < 0) {
-						star.y = height;
-					}
-				}
+    function killShootingStar(shootingStar) {
+      setTimeout(function () {
+        shootingStar.isDying = true;
+      }, shootingStarLifeTime);
+    }
 
-				for (let i = 0; i < shootingStars.length; i += 1) {
-					let shootingStar = shootingStars[i];
-					if (shootingStar.isSpawning) {
-						shootingStar.opacity += shootingStarOpacityDelta;
-						if (shootingStar.opacity >= 1.0) {
-							shootingStar.isSpawning = false;
-							killShootingStar(shootingStar);
-						}
-					}
-					if (shootingStar.isDying) {
-						shootingStar.opacity -= shootingStarOpacityDelta;
-						if (shootingStar.opacity <= 0.0) {
-							shootingStar.isDying = false;
-							shootingStar.isDead = true;
-						}
-					}
-					shootingStar.trailLengthDelta += trailLengthDelta;
+    function update() {
+      context.clearRect(0, 0, width, height);
+      context.fillStyle = "rgba(255, 255, 255, 0)";
+      context.fillRect(0, 0, width, height);
+      context.fill();
+      for (let i = 0; i < stars.length; i += 1) {
+        let star = stars[i];
+        star.update();
+        drawStar(star);
+        if (star.x > width) {
+          star.x = 0;
+        }
+        if (star.x < 0) {
+          star.x = width;
+        }
+        if (star.y > height) {
+          star.y = 0;
+        }
+        if (star.y < 0) {
+          star.y = height;
+        }
+      }
 
-					shootingStar.update();
-					if (shootingStar.opacity > 0.0) {
-						drawShootingStar(shootingStar);
-					}
-				}
+      for (let i = 0; i < shootingStars.length; i += 1) {
+        let shootingStar = shootingStars[i];
+        if (shootingStar.isSpawning) {
+          shootingStar.opacity += shootingStarOpacityDelta;
+          if (shootingStar.opacity >= 1.0) {
+            shootingStar.isSpawning = false;
+            killShootingStar(shootingStar);
+          }
+        }
+        if (shootingStar.isDying) {
+          shootingStar.opacity -= shootingStarOpacityDelta;
+          if (shootingStar.opacity <= 0.0) {
+            shootingStar.isDying = false;
+            shootingStar.isDead = true;
+          }
+        }
+        shootingStar.trailLengthDelta += trailLengthDelta;
 
-				//Delete dead shooting shootingStars
-				for (let i = shootingStars.length - 1; i >= 0; i--) {
-					if (shootingStars[i].isDead) {
-						shootingStars.splice(i, 1);
-					}
-				}
-			}
-			requestAnimationFrame(update);
-		}
+        shootingStar.update();
+        if (shootingStar.opacity > 0.0) {
+          drawShootingStar(shootingStar);
+        }
+      }
 
-		function drawStar(star) {
-			context.fillStyle = "rgba(255, 250, 254,0.75)";
-			context.beginPath();
-			context.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false);
-			context.fill();
-		}
+      //Delete dead shooting shootingStars
+      for (let i = shootingStars.length - 1; i >= 0; i--) {
+        if (shootingStars[i].isDead) {
+          shootingStars.splice(i, 1);
+        }
+      }
+      requestAnimationFrame(update);
+    }
 
-		function drawShootingStar(p) {
-			let x = p.x,
-				y = p.y,
-				currentTrailLength = maxTrailLength * p.trailLengthDelta,
-				pos = lineToAngle(x, y, -currentTrailLength, p.getHeading());
+    function drawStar(star) {
+      context.fillStyle = "rgba(255, 250, 254,0.75)";
+      context.beginPath();
+      context.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false);
+      context.fill();
+    }
 
-			context.fillStyle = "rgba(163, 2, 146, " + p.opacity + ")";
-			let starLength = 5;
-			context.beginPath();
-			context.moveTo(x - 1, y + 1);
+    function drawShootingStar(p) {
+      let x = p.x,
+        y = p.y,
+        currentTrailLength = maxTrailLength * p.trailLengthDelta,
+        pos = lineToAngle(x, y, -currentTrailLength, p.getHeading());
 
-			context.lineTo(x, y + starLength);
-			context.lineTo(x + 1, y + 1);
+      context.fillStyle = "rgba(163, 2, 146, " + p.opacity + ")";
+      let starLength = 5;
+      context.beginPath();
+      context.moveTo(x - 1, y + 1);
 
-			context.lineTo(x + starLength, y);
-			context.lineTo(x + 1, y - 1);
+      context.lineTo(x, y + starLength);
+      context.lineTo(x + 1, y + 1);
 
-			context.lineTo(x, y + 1);
-			context.lineTo(x, y - starLength);
+      context.lineTo(x + starLength, y);
+      context.lineTo(x + 1, y - 1);
 
-			context.lineTo(x - 1, y - 1);
-			context.lineTo(x - starLength, y);
+      context.lineTo(x, y + 1);
+      context.lineTo(x, y - starLength);
 
-			context.lineTo(x - 1, y + 1);
-			context.lineTo(x - starLength, y);
+      context.lineTo(x - 1, y - 1);
+      context.lineTo(x - starLength, y);
 
-			context.closePath();
-			context.fill();
+      context.lineTo(x - 1, y + 1);
+      context.lineTo(x - starLength, y);
 
-			//trail
-			context.fillStyle = "rgba(163, 2, 146, " + p.opacity + ")";
-			context.beginPath();
-			context.moveTo(x - 1, y - 1);
-			context.lineTo(pos.x, pos.y);
-			context.lineTo(x + 1, y + 1);
-			context.closePath();
-			context.fill();
-		}
+      context.closePath();
+      context.fill();
 
-		//Run
-		update();
+      //trail
+      context.fillStyle = "rgba(163, 2, 146, " + p.opacity + ")";
+      context.beginPath();
+      context.moveTo(x - 1, y - 1);
+      context.lineTo(pos.x, pos.y);
+      context.lineTo(x + 1, y + 1);
+      context.closePath();
+      context.fill();
+    }
 
-		//Shooting stars
-		setInterval(function () {
-			if (paused) return;
-			createShootingStar();
-		}, shootingStarEmittingInterval);
+    //Run
+    update();
 
-		function resume() {
-			paused = false;
-		}
+    //Shooting stars
+    setInterval(function () {
+      createShootingStar();
+    }, shootingStarEmittingInterval);
+  }, [windowInnerWidth]);
 
-		function pause() {
-			paused = true;
-		}
-
-		window.addEventListener("focus", resume);
-		window.addEventListener("blue", pause);
-
-		return () => {
-			window.removeEventListener("focus", resume);
-			window.removeEventListener("blue", pause);
-		};
-	}, []);
-
-	return null;
+  return null;
 };
 
 export default useBannerBackground;
