f68438f4ba97cf14afc9de1091ee3fcfeeab89bf
Denny Hong <hong961127@gmail.com>
2021-05-29 18:28:20 -0700

    Upgrade to webpack5

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
+	future: {
+		webpack5: true,
+	},
+
 	webpack: (config, { isServer }) => {
 		// Fixes npm packages that depend on `fs` module
 		if (!isServer) {
-			config.node = {
-				fs: "empty",
-			};
+			config.resolve.fallback.fs = false;
 		}
-
 		return config;
 	},
 	images: {
